46467c33a0be0093ec76cb4fdd8ca33e6aa8e21f
Derek Lorenzen <derek@digitalassetsdata.com>
2019-11-11 21:33:49 -0700

    update

--- run.py
+++ run.py
@@ -18,3 +18,22 @@ wanted={'binance',
 with open('test3.txt', 'a') as f:
     cg = CoingeckoAPI(filepath, f=f, wanted=wanted)
     cg.run()
+
+'''
+Testing:
+Data seems only available back to Sept. 9th or 22nd 2019 most times
+Binance_US drops to 2 volume at Sept. 23 2019
+Short dates: Binance, 
+             Bitfinex, 
+             Binance, 
+             bitstamp, 
+             bittrex, 
+             gdax (22nd), 
+             gemini (28th), 
+             huobi (27th), 
+             itbit, 
+             okex, 
+             poloniex
+Long Dates: Bitflyer 11-12-2018 to present, 
+            Kraken 11-13-2018
+'''
